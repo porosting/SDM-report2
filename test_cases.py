3afa23f8ba4b152c0eda2a5e571fed24e2e0cf8f
porosting <p.name_porost009@icloud.com>
2026-02-07 11:39:15 +0900

    add

--- test_cases.py
+++ test_cases.py
@@ -17,14 +17,16 @@ class TestCalc (unittest.TestCase):
                 self.assertEqual (-1, calc('a','b'))
         def test_sample4 (self):
                 self.assertEqual (-1, calc(0.1,999))
-                                         
+
         def test_sample5 (self):
                 self.assertEqual (-1, calc(0,0))
         def test_sample6 (self):
-                self.assertEqual (1000000, calc(1000,1000))
+                self.assertEqual (-1, calc(999,1000))
         def test_sample7 (self):
                 self.assertEqual (998001, calc(999,999))
         def test_sample8 (self):
                 self.assertEqual (-1, calc("5",10))
         def test_sample9 (self):
+                self.assertEqual (-1, calc("5","10"))
+        def test_sample10 (self):
                 self.assertEqual (-1, calc(1.0,1))
