3d268e1d3b3be47b7f63e17e93fb15704890293a
porosting <p.name_porost009@icloud.com>
2026-02-07 19:27:55 +0900

    change

--- calc_mul.py
+++ calc_mul.py
@@ -3,12 +3,9 @@
 import re
                 
 def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile(r'^\d+$')
-        if p.match(ai) and p.match(bi):
-                a=float(ai)
-                b=float(bi)
+        if isinstance(A, int) and isinstance(B, int):
+                a=A
+                b=B
                 if 1 <= a and 1<=b and a<=999 and b<=999:
                         valid=True
                 else:
